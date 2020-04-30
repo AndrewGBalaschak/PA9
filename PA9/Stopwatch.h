a8f040f15a46767ad27959baccdc47bb1ca7c247
scn1901 <33670707+scn1901@users.noreply.github.com>
2020-04-29 19:19:40 -0700

    consolidate cpp to h

--- PA9/Stopwatch.h
+++ PA9/Stopwatch.h
@@ -8,5 +8,13 @@ public:
 	Stopwatch(int m, int s) :Timer(m, s) {
 		prevRevival = start;
 	}
-	void aliveTime();
+	void Stopwatch::aliveTime() {
+		time_t current;
+		time(&current);
+		int totSec = 0;
+		totSec = current - prevRevival;
+
+		minutes = totSec / 60;
+		seconds = totSec % 60;
+	}
 };
