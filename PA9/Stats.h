7e16991bb4d015ca2f896658a365e8f9c53f79eb
scn1901 <33670707+scn1901@users.noreply.github.com>
2020-05-01 14:55:19 -0700

    Update Stats.h

--- PA9/Stats.h
+++ PA9/Stats.h
@@ -17,14 +17,14 @@ public:
 		fuelText = new sf::Text("Fuel: ", font);
 		scoreText = new sf::Text("Score: ", font);
 
-		fuelText->setPosition(width - 80, 50);
-		fuelText->setCharacterSize(15);
-		scoreText->setPosition(width - 80, 70);
-		scoreText->setCharacterSize(15);
-		fuelNum->setPosition(width - 30, 50);
-		fuelNum->setCharacterSize(15);
-		scoreNum->setPosition(width - 30, 70);
-		scoreNum->setCharacterSize(15);
+		fuelText->setPosition(width - 80, 20);
+		fuelText->setCharacterSize(30);
+		scoreText->setPosition(width - 80, 40);
+		scoreText->setCharacterSize(30);
+		fuelNum->setPosition(width - 30, 20);
+		fuelNum->setCharacterSize(30);
+		scoreNum->setPosition(width - 30, 40);
+		scoreNum->setCharacterSize(30);
 	}
 	void updateStats(Player *p) {
 		fuel = p->getFuel();
