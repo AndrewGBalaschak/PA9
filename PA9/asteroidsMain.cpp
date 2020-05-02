1e558852827b74239178df65dd6a1d31553fb1e2
svetstufff <63481479+svetstufff@users.noreply.github.com>
2020-05-01 18:52:07 -0700

    Create asteroidsMain.cpp

---
+++ PA9/asteroidsMain.cpp
@@ -0,0 +1,42 @@
+#include "AsteroidsArray.h"
+
+int main(void)
+{
+	srand(time(NULL));
+	RenderWindow window(VideoMode(2000, 2000), "SFML");
+	window.setFramerateLimit(60);
+	Texture texture;
+
+	texture.setSmooth(true);
+	texture.loadFromFile("asteroidTexture.jpg");
+	
+	Asteroid::texture = &texture;
+	Asteroid::window = &window;
+
+	AsteroidsArray asteroidsArray; 
+	asteroidsArray.spawnAsteroid();
+
+	int i = 0;
+
+	while (window.isOpen())
+	{
+		Event event; 
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		i++;
+		window.clear();
+		if (i % 60 == 0)
+		{
+			asteroidsArray.spawnAsteroid();
+		}
+		asteroidsArray.drawAsteroids();
+		window.display();
+	}
+	return 0;
+}
