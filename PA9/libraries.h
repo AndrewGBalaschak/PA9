ec1aa43fa4a459d34e6262f7137ba130cdc0271d
svetstufff <63481479+svetstufff@users.noreply.github.com>
2020-05-03 00:09:43 -0700

    Create libraries.h

---
+++ PA9/libraries.h
@@ -0,0 +1,18 @@
+#ifndef LIBRARIES
+#define LIBRARIES
+
+#include<iostream>
+#define PI 3.141592653589793238462643383279502884197169399375105820974944
+#include <SFML/Graphics.hpp>
+#include <math.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <fstream>
+
+using namespace sf;
+
+#define WIDTH 1000
+#define HEIGHT 1000
+#endif
