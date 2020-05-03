2e0ad823acd362cfef8ea5bba6f05afbdf04bada
svetstufff <63481479+svetstufff@users.noreply.github.com>
2020-05-03 00:11:45 -0700

    Update Stats.h

--- PA9/Stats.h
+++ PA9/Stats.h
@@ -1,40 +1,42 @@
 #ifndef STATS_H
 #define STATS_H
 
+#include "Player.h"
+
 class Stats {
 private:
 	int fuel, score;
-	sf::Text* fuelNum, * scoreNum, *fuelText, *scoreText;
+	sf::Text* fuelNum, * scoreNum, * fuelText, * scoreText;
 	sf::Font font;
 public:
-	Stats(){
+	Stats() {
 		fuel = 500;
 		score = 0;
-		if (!font.loadFromFile("Tuffy.otf")) cout << "ERROR";
+		if (!font.loadFromFile("Tuffy.otf")) std::cout << "ERROR";
 
-		fuelNum = new sf::Text(to_string(0), font);
-		scoreNum = new sf::Text(to_string(0), font);
+		fuelNum = new sf::Text(std::to_string(0), font);
+		scoreNum = new sf::Text(std::to_string(0), font);
 		fuelText = new sf::Text("Fuel: ", font);
 		scoreText = new sf::Text("Score: ", font);
 
-		fuelText->setPosition(width - 80, 50);
+		fuelText->setPosition(WIDTH - 80, 50);
 		fuelText->setCharacterSize(15);
-		scoreText->setPosition(width - 80, 70);
+		scoreText->setPosition(WIDTH - 80, 70);
 		scoreText->setCharacterSize(15);
-		fuelNum->setPosition(width - 30, 50);
+		fuelNum->setPosition(WIDTH - 30, 50);
 		fuelNum->setCharacterSize(15);
-		scoreNum->setPosition(width - 30, 70);
+		scoreNum->setPosition(WIDTH - 30, 70);
 		scoreNum->setCharacterSize(15);
 	}
-	void updateStats(Player *p) {
+	void updateStats(Player* p) {
 		fuel = p->getFuel();
 		score = p->getScore();
 		if (fuel >= 0) {
-			fuelNum->setString(to_string(fuel));
+			fuelNum->setString(std::to_string(fuel));
 		}
-		scoreNum->setString(to_string(score));
+		scoreNum->setString(std::to_string(score));
 	}
-	void drawStats(sf::RenderWindow* win) {
+	void drawStats(RenderWindow* win) {
 		win->draw(*fuelText);
 		win->draw(*scoreText);
 		win->draw(*fuelNum);
