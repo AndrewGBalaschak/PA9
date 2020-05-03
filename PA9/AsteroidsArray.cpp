2d8e9e0c6715d6dd167c42874ed3a27a1cc85cf3
svetstufff <63481479+svetstufff@users.noreply.github.com>
2020-05-03 00:08:01 -0700

    Update AsteroidsArray.cpp

--- PA9/AsteroidsArray.cpp
+++ PA9/AsteroidsArray.cpp
@@ -4,37 +4,39 @@ void AsteroidsArray::drawAsteroids(void)
 {
 	Asteroid* currentAsteroid = nullptr;
 	bool currentDestroyed = 0;
-	
+
 	asteroidsDestroyed = 0;
-	
+
 	for (int i = 0; i < numAsteroids; i++)
 	{
 		currentAsteroid = asteroidsArray[i];
 		currentDestroyed = destroyedAsteroids[i];
-		std::cout << i << ", " << currentDestroyed << std::endl;
+		std::cout << "DESTROYED: " << currentAsteroid << ", " << i << ", " << currentDestroyed << std::endl;
 		if (currentDestroyed == false)
 		{
 			if (currentAsteroid->isOffScreen() == true)
 			{
-				delete currentAsteroid; 
+				delete currentAsteroid;
 				destroyedAsteroids[i] = true;
 			}
-			else if(currentAsteroid->getCollidesPlayer() == true)
+			else if (currentAsteroid->getCollidesPlayer() == true)
 			{
 				gameOver = true;
 			}
-			else if(currentAsteroid->getCollidesBullet() == true)
+			else if (currentAsteroid->getCollidesBullet() == true)
 			{
 				asteroidsDestroyed++;
-				delete currentAsteroid; 
+				delete currentAsteroid;
 				destroyedAsteroids[i] = true;
 			}
 			else
 			{
+				std::cout << "CURRENT_ASTEROID" << currentAsteroid;
+
 				currentAsteroid->move();
 				currentAsteroid->drawAsteroid();
 			}
-			
+
 		}
 
 	}
@@ -44,8 +46,8 @@ void AsteroidsArray::drawAsteroids(void)
 Asteroid* AsteroidsArray::spawnAsteroid(void)
 {
 	Asteroid* newAsteroid = new Asteroid;
-	asteroidsArray = (Asteroid**) realloc(asteroidsArray, ++numAsteroids * sizeof(Asteroid *));
-	destroyedAsteroids = (bool*)realloc(destroyedAsteroids, numAsteroids * sizeof(bool));
+	asteroidsArray = (Asteroid **) realloc(asteroidsArray, ++numAsteroids * sizeof(Asteroid*));
+	destroyedAsteroids = (bool*) realloc(destroyedAsteroids, numAsteroids * sizeof(bool));
 
 	destroyedAsteroids[numAsteroids - 1] = false;
 	asteroidsArray[numAsteroids - 1] = newAsteroid;
