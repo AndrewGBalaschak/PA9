83a7b6de0cbb6e178ab6657b88bee5f8a673267a
svetstufff <63481479+svetstufff@users.noreply.github.com>
2020-05-03 21:58:12 -0700

    Update AsteroidsArray.cpp

--- PA9/AsteroidsArray.cpp
+++ PA9/AsteroidsArray.cpp
@@ -5,16 +5,14 @@ void AsteroidsArray::drawAsteroids(void)
 	Asteroid* currentAsteroid = nullptr;
 	bool currentDestroyed = 0;
 	
-	//std::cout << "NUM_ASTEROIDS: " << numAsteroids << std::endl;
-
+	//iterate through all asteroids that have been spawned
 	for (int i = 0; i < numAsteroids; i++)
 	{
-		currentAsteroid = asteroidsArray[i];
-		currentDestroyed = destroyedAsteroids[i];
-		//std::cout << "DESTROYED: " << currentAsteroid << ", " << i << ", " << currentDestroyed << std::endl;
-		if (currentDestroyed == false)
+		currentAsteroid = asteroidsArray[i]; //get the current asteroid
+		currentDestroyed = destroyedAsteroids[i]; 
+		if (currentDestroyed == false) //if the current asteroid has already been destroyed, then don't do anything
 		{
-			if (currentAsteroid->isOffScreen() == true)
+			if (currentAsteroid->isOffScreen() == true) //if asteroid is offscreen or has collided with a bullet or player, destroy the asteroid and make the appropriate changes
 			{
 				currentAsteroid->isDead();
 				destroyedAsteroids[i] = true;
@@ -33,8 +31,6 @@ void AsteroidsArray::drawAsteroids(void)
 			}
 			else
 			{
-				//std::cout << "CURRENT_ASTEROID" << currentAsteroid;
-
 				currentAsteroid->move();
 				if (currentAsteroid->getActive())
 					currentAsteroid->drawAsteroid();
@@ -51,11 +47,11 @@ void AsteroidsArray::drawAsteroids(void)
 
 }
 
-Asteroid* AsteroidsArray::spawnAsteroid(void)
+Asteroid* AsteroidsArray::spawnAsteroid(void) 
 {
-	Asteroid* newAsteroid = new Asteroid;
-	asteroidsArray = (Asteroid **) realloc(asteroidsArray, ++numAsteroids * sizeof(Asteroid*));
-	destroyedAsteroids = (bool*) realloc(destroyedAsteroids, numAsteroids * sizeof(bool));
+	Asteroid* newAsteroid = new Asteroid; //dynamically allocate a new asteroid
+	asteroidsArray = (Asteroid **) realloc(asteroidsArray, ++numAsteroids * sizeof(Asteroid*)); //increase the size of the asteroids array by one to make room for the new asteroid
+	destroyedAsteroids = (bool*) realloc(destroyedAsteroids, numAsteroids * sizeof(bool)); //same for destroyed asteroids array
 
 	destroyedAsteroids[numAsteroids - 1] = false;
 	asteroidsArray[numAsteroids - 1] = newAsteroid;
@@ -78,7 +74,6 @@ void AsteroidsArray::checkForCollisions(std::vector<Projectile *> &objs, Player 
 	for (int i = 0; i < numAsteroids; i++) {
 		currentAsteroid = asteroidsArray[i];
 		currentDestroyed = destroyedAsteroids[i];
-		//std::cout << "DESTROYED: " << currentAsteroid << ", " << i << ", " << currentDestroyed << std::endl;
 		if (currentDestroyed == false) {
 			currentAsteroid->collides_(&p);
 			for (int i = 0; i < objs.size(); i++) {
